c9bcae515a7478514cb620572bee8da7ede743a5
Matheus-Lanzieri <matheus.lanzieri@outlook.com>
2026-08-22 11:40:07 -0300

    array bidimensional

---
+++ Aula02/matriz1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int matriz2d[3][3]; //[][][]
+                        //[][][]
+                        //[][][]
+    for(int linha = 0; linha < 3; linha++)
+    {
+        for(int col = 0; col < 3; col++)
+        {
+            printf("Entre com o valor: ");
+            scanf("%d", matriz2d[linha][col])
+        }//fim da coluna
+    }//fim da linha
+    return 0;
+}

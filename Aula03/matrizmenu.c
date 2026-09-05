a58b51814fef95eed0f45f66a07eae4903a0c7f4
Matheus-Lanzieri <matheus.lanzieri@outlook.com>
2026-09-05 10:32:34 -0300

    correção do código

--- Aula03/matrizmenu.c
+++ Aula03/matrizmenu.c
@@ -2,7 +2,7 @@
 int main()
 {
     float matrizMenu[10][4];
-    int op = 1, linha = 0;
+    int op = 1, linha = 0, qtdItens = 0;
     
     while(op > 0)
     {
@@ -14,33 +14,51 @@ int main()
         printf("\n -- 3: Paçoquita -- R$ 2,00- \n");
         printf("\n -- 4: Pao queijo --R$ 5,00- \n");
         printf("\n escolha o item pelo numero ou 0 para sair: ");
-        scanf("%d", $op);
+        scanf("%d", &op);
         
         switch(op)
         {
-            case:1
+            case 1:
                 matrizMenu[linha][0] = 1;
                 matrizMenu[linha][1] = 6.50;
                 printf ("\n Quantas coca colas você deseja?: ");
                 scanf("%f", &matrizMenu[linha][2]);
-                matrizMenu[linha3][3] = matrizMenu[linha][1] * matrizMenu[linha][2];
+                matrizMenu[linha][3] = matrizMenu[linha][1] * matrizMenu[linha][2];
                 linha++;
             break;
-            case:2
-            
+            case 2:
+                matrizMenu[linha][0] = 2;
+                matrizMenu[linha][1] = 27.50;
+                printf ("\n Quantos x saladas você deseja?: ");
+                scanf("%f", &matrizMenu[linha][2]);
+                matrizMenu[linha][3] = matrizMenu[linha][1] * matrizMenu[linha][2];
+                linha++;
             break;
-            case:3
-            
+            case 3:
+                matrizMenu[linha][0] = 3;
+                matrizMenu[linha][1] = 2.00;
+                printf ("\n Quantas paçoquitas você deseja?: ");
+                scanf("%f", &matrizMenu[linha][2]);
+                matrizMenu[linha][3] = matrizMenu[linha][1] * matrizMenu[linha][2];
+                linha++;
             break;
-            case:4
-            
+            case 4:
+                matrizMenu[linha][0] = 4;
+                matrizMenu[linha][1] = 5.00;
+                printf ("\n Quantas pao de queijo você deseja?: ");
+                scanf("%f", &matrizMenu[linha][2]);
+                matrizMenu[linha][3] = matrizMenu[linha][1] * matrizMenu[linha][2];
+                linha++;
             break;
             default:
-            
+                printf("\n Escolha uma opção valida! ");
             break;
         }
         
     }
-    
+    for(linha = 0; linha < 10; linha++)
+    {
+        printf("\n|%f  |R$ %f  | %f  |R$ %f  |", matrizMenu[linha][0], matrizMenu[linha][1], matrizMenu[linha][2], matrizMenu[linha][3] );
+    }
     return 0;
-}
+}//fim do main

77096135cab66ff827faba2a3e4f064a4406eb0b
Matheus-Lanzieri <matheus.lanzieri@outlook.com>
2026-09-05 12:03:42 -0300

    trabalhando com funções

---
+++ Aula03/funcoes.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+//declarando as funções:
+void seApresentar();
+float retornaSoma();
+
+int main()
+{
+    //Invocando a função seApresentar():
+    seApresentar();
+    //A variável resultado guarda o que vier da função retornaSoma
+    float resultado = retornaSoma();
+    
+    // Mostrando conteúdo da variavel resultado:
+    printf("\nResultado da soma: %.2f", resultado);
+
+    return 0;
+}//fim da main
+
+float retornaSoma()
+{
+    float numA=0.0, numB = 0.0, result = 0.0;
+    printf("\nDigite o primeiro numero: ");
+    scanf("%f", &numA);
+    printf("\nDigite segundo numero: ");
+    scanf("%f", &numB);
+    
+    result = numA + numB;
+    
+    return result;
+    
+}//fim retornaSoma
+
+void seApresentar()
+{
+    printf("\n Olá, sou Eu oh!");
+    
+    //percebam que não tem o return pois a função tem
+    //o tipo de retorno definido como vazio (void)
+    
+}// fim da seApresentar

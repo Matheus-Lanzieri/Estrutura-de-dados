2cc5dfd29185942d929b189bc73b578259bbef97
Matheus Lanzieri <matheus.lanzieri@outlook.com>
2026-09-12 10:29:24 -0300

    correção no código

--- Aula04/bubblesort.c
+++ Aula04/bubblesort.c
@@ -2,12 +2,12 @@
 
 void bubbleSort(int vetor[], int tamanho)
 {
-    for (int i=0; i < tamanho - 1; i++){
+    for (int i = 0; i < tamanho - 1; i++) {
         for (int j = 0; j < tamanho - i - 1; j++) {
-            if (vetor[j] > vetor[j+1]) {
-            int temp = vetor[j];
-            vetor[j] = vetor [j+1];
-            vetor[j+1] = temp;
+            if (vetor[j] > vetor[j + 1]) {
+                int temp = vetor[j];
+                vetor[j] = vetor[j + 1];
+                vetor[j + 1] = temp;
             }
         }
     }
@@ -16,14 +16,13 @@ void bubbleSort(int vetor[], int tamanho)
 int main()
 {
     int vetor[5] = {8, 4, 5, 9, 1};
-    int i;
     int tamanho = 5;
-    
+
     bubbleSort(vetor, tamanho);
-    
-    for (i = 0; i < tamanho, i++){
-        printf("%d", vetor[i]);
+
+    for (int i = 0; i < tamanho; i++) {
+        printf("%d ", vetor[i]);
     }
-    
+
     return 0;
 }
